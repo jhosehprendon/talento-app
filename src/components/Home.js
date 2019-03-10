7ab07a741c40225406506ab98faffb236681f1db
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2019-03-09 23:43:05 -0500

    Worked responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,60 +13,50 @@ const  Home = () => {
                 </div>
                 </div>
             </ul>
-            <div className="ui equal width aligned padded grid">
-                <div className="row" style={{marginTop: '80px'}}>
-                    <div className="column" style={{marginTop:'120px', marginLeft: '15%'}}>
+            <div style={{marginTop:'16%'}} className="ui stackable three column grid">
+                    <div className="column" style={{ marginLeft: '20%'}}>
                         <h1>Super powers for Recruiters</h1>
                         <h3>Softhunt helps recruiters build awesome job listings and manage their candidates in one place</h3>
                         <button style={{marginTop:'5px'}} className="ui large button primary">Start Free</button>
                     </div>
                     <div className="column">
-                        <img style={{width: '60%', marginTop: '120px'}} src={require('../images/background.png')} />
+                        <img style={{width: '70%'}} src={require('../images/background.png')} />
                     </div>
-
-                </div>
             </div>
             <div>
                 <div style={{ width: '30%', margin: '180px auto 50px auto'}} className="ui section divider"></div>
                 <h1 style={{textAlign: 'center'}}>How it works</h1>
-                <div className="ui equal width aligned padded grid">
-                    <div className="row">
-                        <div className="column" style={{marginTop:'120px', marginLeft: '15%'}}>
+                <div className="ui stackable three column grid">
+                        <div className="column" style={{marginTop:'120px', marginLeft: '25%'}}>
                             <h1>Create a job list</h1>
-                            <h3>Add new job positions with descriptions and specifications. <br/> Create a professional looking listing where candidates can apply</h3>
+                            <h3>Add new job positions with descriptions and specifications. Create a professional looking listing where candidates can apply</h3>
                         </div>
                         <div className="column">
                             <img style={{width: '40%', marginTop: '120px'}} src={require('../images/1.png')} />
                         </div>
 
-                    </div>
                 </div>
                 <div style={{ width: '30%', margin: '100px auto 20px auto'}} className="ui section divider"></div>
 
-                <div className="ui equal width aligned padded grid">
-                    <div className="row">
+                <div style={{marginLeft: '25%'}} className="ui stackable three column grid">
                         <div className="column">
-                            <img style={{width: '40%', marginTop: '120px', marginLeft: '45%'}} src={require('../images/2.png')} />
+                            <img style={{width: '65%', marginTop: '120px'}} src={require('../images/2.png')} />
                         </div>
                         <div className="column" style={{marginTop:'120px'}}>
                             <h1>Manage all in a dashboard</h1>
-                            <h3>Have control of every candidate that applies to each position. <br/> Set tasks and track progress on each candidate</h3>
+                            <h3>Have control of every candidate that applies to each position. Set tasks and track progress on each candidate</h3>
                         </div>
-                    </div>
                 </div>
                 <div style={{ width: '30%', margin: '100px auto 20px auto'}} className="ui section divider"></div>
 
-                <div style={{paddingBottom: '100px'}} className="ui equal width aligned padded grid">
-                    <div className="row">
-                        <div className="column" style={{marginTop:'120px', marginLeft: '15%'}}>
+                <div style={{paddingBottom: '100px'}} className="ui stackable three column grid">
+                        <div className="column" style={{marginTop:'120px', marginLeft: '25%'}}>
                             <h1>Team work</h1>
-                            <h3>Assign tasks to team members, organize information, manage <br/> progress, communicate and share notes.</h3>
+                            <h3>Assign tasks to team members, organize information, manage progress, communicate and share notes.</h3>
                         </div>
                         <div className="column">
                             <img style={{width: '40%', marginTop: '120px'}} src={require('../images/3.png')} />
                         </div>
-
-                    </div>
                 </div>
                 
             </div>
